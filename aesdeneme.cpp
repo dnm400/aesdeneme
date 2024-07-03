01848c834dcaedfd81b9b60a05f35c47fb6698c9
Sumeyye Aydogan <sumeyyednm1@gmail.com>
2024-07-03 10:40:57 +0300

    invshiftrows

--- aesdeneme.cpp
+++ aesdeneme.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 
 void ShiftRows(vector<vector<char>>& newst) { //vector of vectors provides matrix
-    //second row, one shift
+    //second row, one left shift
     vector<vector<char>> temp = newst;
     newst[1][0] = temp[1][1];
     newst[1][1] = temp[1][2];
@@ -24,6 +24,26 @@ void ShiftRows(vector<vector<char>>& newst) { //vector of vectors provides matri
     newst[3][2] = temp[3][1];
     newst[3][3] = temp[3][2];
 }
+
+void invShiftRows(vector<vector<char>>& newst) { //vector of vectors provides matrix
+    //second row, one right shift
+    vector<vector<char>> temp = newst;
+    newst[1][0] = temp[1][3];
+    newst[1][1] = temp[1][0];
+    newst[1][2] = temp[1][1];
+    newst[1][3] = temp[1][2];
+    //third row, two shift
+    newst[2][0] = temp[2][2];
+    newst[2][1] = temp[2][3];
+    newst[2][2] = temp[2][0];
+    newst[2][3] = temp[2][1];
+    //fourth row, three shift
+    newst[3][0] = temp[3][1];
+    newst[3][1] = temp[3][2];
+    newst[3][2] = temp[3][3];
+    newst[3][3] = temp[3][0];
+}
+
 
 void RotWord(vector<char>& rotw){
     char temp = rotw[0];
